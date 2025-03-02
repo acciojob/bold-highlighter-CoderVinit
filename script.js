308a4c1ca186e37d4e6a1ee71b7142a493a51883
VINIT KUMAR PATEL <101181057+CoderVinit@users.noreply.github.com>
2025-03-02 09:30:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 function highlight() {
-    //Write your code here
-
-
+    document.querySelectorAll("strong").forEach(element => {
+        element.style.color = "rgb(0, 128, 0)"; // Change to green
+    });
 }
 
-
 function return_normal() {
-    //Write your code here
-
-    
+    document.querySelectorAll("strong").forEach(element => {
+        element.style.color = "rgb(0, 0, 0)"; // Revert to black
+    });
 }
